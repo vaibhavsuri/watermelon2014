ceb56e5e4891c1bb3217116f84c5eb969dead343
burakfirik <burakfirik@gmail.com>
2014-10-29 13:03:53 -0400

    Updated simulator

--- Watermelon/watermelon/sim/seed.java
+++ Watermelon/watermelon/sim/seed.java
@@ -7,6 +7,7 @@ public class seed {
     public double x;
     public double y;
     public boolean tetraploid;
+    public double score;
     
     public seed() { x = 0.0; y = 0.0; tetraploid = false; }
 
