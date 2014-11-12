71a2cd253c4281643bdefddd5eb77f48eee1738d
burakfirik <burakfirik@gmail.com>
2014-11-12 10:25:57 -0500

    fixed incremental coloring

--- Watermelon/watermelon/group5/Player.java
+++ Watermelon/watermelon/group5/Player.java
@@ -37,21 +37,21 @@ public class Player extends watermelon.sim.Player{
 		double bestScore = 0;
 		ArrayList<seed> bestList = null;
 		packingList.put(Packing.hexagonal(treelist, length, width),"hexagonal");
-		packingList.put(Packing.hexagonal_invert(treelist, length, width),"hexagonal_invert");
+		/*packingList.put(Packing.hexagonal_invert(treelist, length, width),"hexagonal_invert");
 		packingList.put(Packing.horizontal(treelist, length, width),"horizontal");
 		packingList.put(Packing.horizontal_mind_trees(treelist, length, width),"horizontal_mind_trees");
 		packingList.put(Packing.physicalWithExisting(treeSeed, null, width, length),"Physcal Packing");
-		for (java.util.Map.Entry<ArrayList<seed>, String> packList : packingList.entrySet()){
-			finalSolutionList.put(Coloring.neighbor_invert_topleft(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: neighbor_invert_topleft");
+		*/for (java.util.Map.Entry<ArrayList<seed>, String> packList : packingList.entrySet()){
+			//finalSolutionList.put(Coloring.neighbor_invert_topleft(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: neighbor_invert_topleft");
 			finalSolutionList.put(Coloring.incrementalColoring(packList.getKey(), length, width, s),"Packing: "+packList.getValue()+"\tColoring: incrementalColoring");
-			finalSolutionList.put(Coloring.alternate(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: alternate");
+			/*finalSolutionList.put(Coloring.alternate(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: alternate");
 			finalSolutionList.put(Coloring.alternate_edge_start(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: alternate_edge_start");
 			finalSolutionList.put(Coloring.concentric(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: concentric");
 			finalSolutionList.put(Coloring.randomColoring(packList.getKey()),"random");
 			finalSolutionList.put(Coloring.neighbor_invert(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: neighbor_invert");
 			finalSolutionList.put(Coloring.insideout(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: insideout");
 			finalSolutionList.put(Coloring.topleft(packList.getKey(), s, length, width),"Packing: "+packList.getValue()+"\tColoring: topleft");
-			
+			*/
 		}
 		
 		
